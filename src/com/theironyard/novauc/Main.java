279ce283bfa1e56844d5c908f36a4f4e88978243
Craigslist Buyer <craigslistbuyer@craigslists-mbp.nvstu.edu>
2017-02-08 17:53:06 -0500

    ATM Project Complete

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -1,8 +1,42 @@
 package com.theironyard.novauc;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        // write your code here
+
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        System.out.println("Welcome, " + name);
+
+        System.out.println("What would you like to do?");
+
+        System.out.println("Check my balance/Withdraw Funds/Cancel]");
+        String choice = scanner.nextLine();
+
+
+        if (choice.equalsIgnoreCase("Check my balance")) {
+            System.out.println("Current Balance");
+        }
+        if (choice.equalsIgnoreCase(" Withdraw Funds")) {
+            System.out.println("print balance!");
+        } else if (choice.equalsIgnoreCase("Cancel")) {
+            System.out.println("Thank you and please come again!");
+            throw new Exception("Invalid Choice.");
+        } else if (choice.equalsIgnoreCase("Cancel")) {
+            System.out.println("Deposit Nothing");
+            throw new Exception("Invalid Choice.");
+
+        }
     }
 }
+
+
+
+
+
+
